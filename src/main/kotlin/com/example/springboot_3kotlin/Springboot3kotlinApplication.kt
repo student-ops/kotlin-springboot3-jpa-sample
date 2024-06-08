7400e43b9722705c56f0d7d35a932fc2cdc99a1a
student-ops <student-ops@example.com>
2024-06-09 02:39:42 +0900

    initial commit

---
+++ src/main/kotlin/com/example/springboot_3kotlin/Springboot3kotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboot_3kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springboot3kotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<Springboot3kotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springboot_3kotlin/Springboot3kotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springboot_3kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springboot3kotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
