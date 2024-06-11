ccafac65bb7aa9df063777fec21e2131e3ead5a1
student-ops <student-ops@example.com>
2024-06-12 00:53:35 +0900

    update Calling Implementation of Interface

--- src/main/kotlin/com/example/springboot_3kotlin/MentionController.kt
+++ src/main/kotlin/com/example/springboot_3kotlin/MentionController.kt
@@ -2,19 +2,22 @@ package com.example.springboot_3kotlin
 
 import com.linecorp.bot.model.event.MessageEvent
 import com.linecorp.bot.model.event.message.MessageContent
+import com.linecorp.bot.model.event.message.TextMessageContent
 import com.linecorp.bot.model.message.Message
 import com.linecorp.bot.model.message.TextMessage
 import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
 import org.springframework.stereotype.Controller
 
+
 @LineMessageHandler
 @Controller
 class MessageController {
 
     @EventMapping
-    fun handleMessage(event: MessageEvent<MessageContent>): Message {
-        return TextMessage("メッセージ受信しました")
+    fun handleMessage(event: MessageEvent<TextMessageContent>): Message {
+        val messageText = event.message.text
+        return TextMessage("あなたが送信したメッセージ: " + messageText);
     }
 
 }
